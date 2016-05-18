b89b193b74854bbdd03cfe1390e72a7340096a63
rafael reis <orafaelreis@Mac-mini-de-rafael.local>
2016-05-18 04:35:51 -0300

    página de contato refeita

--- js/submitform.js
+++ js/submitform.js
@@ -4,8 +4,6 @@ $('#formcontato').submit(function(e){
     var valid = validaForm(this);
     if (valid) {
       var url = 'http://rocky-ocean-9250.heroku.com/scripts-php/send-mail-to-gglobalseguros.php?access=1';
-      // var url = 'http://localhost:8888/heroku/php-getting-started/web/scripts-php/send-mail-to-gglobalseguros.php?access=1';
-      // var url = 'http://orafaelreis.web2319.uni5.net/send-mail-to-gglobalseguros.php?access=1';
 
       var data = {
         'fnome': this.fnome.value,
@@ -16,45 +14,23 @@ $('#formcontato').submit(function(e){
         'fcodigo': this.fcodigo.value
       };
 
-      // $.ajax({
-      //   async: true,
-      //   type: "POST",
-      //   url: url,
-      //   crossDomain: true,
-      //   contentType: "application/json;",
-      //   // dataType: 'json',
-      //   // data: JSON.stringify(data),
-      //   success: function(data){
-      //     $('#form_warning').addClass('success').html(data.success_message);
-      //   },
-      //   error: function(data){
-      //     data = data.responseJSON;
-      //     $('#form_warning').addClass('error').html(data.error_message);
-      //   },
-      // });
-
-      var data = null;
-
-  var xhr = new XMLHttpRequest();
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-      console.log(this.responseText);
-    }
-  });
-
-  xhr.open("OPTIONS", "https://rocky-ocean-9250.heroku.com/scripts-php/send-mail-to-gglobalseguros.php?access=1");
-  xhr.setRequestHeader("cache-control", "no-cache");
-  xhr.send(data);
-
+      $.ajax({
+        async: true,
+        type: "POST",
+        url: url,
+        contentType: "application/json;",
+        data: JSON.stringify(data),
+        success: function(data, b){
+          $('#form_warning').addClass('success').html(data.success_message);
+        },
+        error: function(data, b){
+          data = data.responseJSON;
+          $('#form_warning').addClass('error').html(data.error_message);
+        },
+      });
 
       $('#formcontato').remove();
       $('#form_warning').attr('class','').html('Enviando...');
-      // $.post( url, , )
-      // .done(function (data) {
-      // }).fail(function(data) {
-      //   $('#form_warning').html('<p class="error"' + data.error_message + '</p>');
-      // });
     }
 
     return false;
